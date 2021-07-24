8fae10d6c8b71b5ba4384328666fecfc2129c032
joannakoay <joannakoay@hotmail.com>
2021-07-24 23:47:27 +0800

    initial version

---
+++ Nlog/Nlog/Program.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using NLog.Fluent;
+using System;
+
+namespace Nlog
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            logger.Error("Error message");
+            logger.Debug("Debug message");
+            logger.Info("Info message");
+
+            try
+            {
+                throw new ArgumentException("Argument Exception");
+            }
+            catch(Exception ex)
+            {
+                logger.ErrorException("Error Exception", ex);
+            }
+
+            Console.Read();
+        }
+    }
+}
